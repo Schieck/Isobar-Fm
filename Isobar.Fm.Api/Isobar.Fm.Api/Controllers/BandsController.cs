72dc939600aa4deac337d7c99c7c65fa26049e34
Schieck <ricardoschieck@gmail.com>
2019-06-08 16:17:01 -0300

    First Commit

---
+++ Isobar.Fm.Api/Isobar.Fm.Api/Controllers/BandsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Isobar.Fm.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BandsController : ControllerBase
+    {
+        // GET api/bands
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/bands/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/bands
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/bands/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/bands/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Isobar.Fm.Api/Isobar.Fm.Api/Startup.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Isobar.Fm.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Services
+
+            // Repositories 
+
+            // Health Check
+            services.AddHealthChecks();
+
+            // Swagger
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Isobar Fm Api",
+                    Description = "An api to some external services for our fm organization radio.",
+                    Contact = new Contact
+                    {
+                        Email = "ricardoschieck@gmail.com",
+                        Name = "Ricardo Schieck",
+                        Url = "https://www.linkedin.com/in/ricardo-schieck-9b9017143/"
+                    }
+                });
+            });
+
+            // Automapper
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseCors(x => x.AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .AllowAnyOrigin()
+               )
+               .UseHealthChecks("/api/HealthCheck")
+               .UseSwagger()
+               .UseSwaggerUI(options =>
+               {
+                   options.SwaggerEndpoint("v1/swagger.json", "Isobar Fm Api");
+               })
+               .UseHttpsRedirection()
+               .UseMvc();
+        }
+    }
+}
